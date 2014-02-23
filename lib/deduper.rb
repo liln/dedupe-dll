9f4dc1424e0f3c9dc3c5cdb85910da969c1835f8
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-22 18:33:16 -0800

    cleaner deduper

--- lib/deduper.rb
+++ lib/deduper.rb
@@ -3,13 +3,13 @@ module Deduper
     count = Array.new(100, 0) # assume only numbers 1 - 100
     n1 = list.head
     while n1 do
-      count[n1.value - 1] = count[n1.value - 1] + 1
+      count[n1.value - 1] += 1
       n1 = n1.nexxt
     end
 
     n1 = list.tail
     while n1 do
-      count[n1.value - 1] = count[n1.value - 1] - 1
+      count[n1.value - 1] -= 1
       if count[n1.value - 1] > 0
         n2 = n1
         n1 = n1.prev
